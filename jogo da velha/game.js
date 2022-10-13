39da83de591e39373cf06bbd98e48a696b29d16e
Fernando Silva França <88167991+Fer-nam@users.noreply.github.com>
2022-10-13 15:58:03 -0300

    Segundo commit - ainda precisa fazer funcionar

---
+++ jogo da velha/game.js
@@ -0,0 +1,14 @@
+let board = ['','','','','','','','',''];
+let playTime = 0;
+let symbols = ['o', 'x'];
+
+
+function handleMove(position){
+    board[position] = symbols[playTime];
+
+    if(playTime == 0){
+        playTime = 1;
+    }else{
+        playTime = 0;
+    }
+}
